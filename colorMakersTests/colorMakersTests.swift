c4074ad83e478e7b3f1bc164db5cccea454e42c9
Yaremith  Damian <laudamian27@gmail.com>
2020-01-31 23:34:40 -0600

    Initial Commit

---
+++ colorMakersTests/colorMakersTests.swift
@@ -0,0 +1,34 @@
+//
+//  colorMakersTests.swift
+//  colorMakersTests
+//
+//  Created by Laura Yaremith Damian  Padilla on 31/01/20.
+//  Copyright © 2020 Laura Yaremith Damian  Padilla. All rights reserved.
+//
+
+import XCTest
+@testable import colorMakers
+
+class colorMakersTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
